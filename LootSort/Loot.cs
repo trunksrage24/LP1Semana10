53289ef01c8a46653403c7c2912142132e6f235d
Vitor <vitor.daniel@outlook.pt>
2023-05-19 12:10:20 +0100

    checked every file for errors and ran program

--- LootSort/Loot.cs
+++ LootSort/Loot.cs
@@ -1,98 +1,103 @@
-namespace LootSort
-{
-    /// <summary>
-    /// The Loot class should implement IComparable<Loot>
-    /// and override GetHashCode() and Equals()
-    /// </summary>
-    public class Loot : ICompare<Loot>
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+    namespace LootSort
     {
-        /// <summary>Type of loot.</summary>
-        public LootType Kind { get; }
-
-        /// <summary>Loot description.</summary>
-        public string Description { get; }
-
-        /// <summary>Loot value.</summary>
-        public float Value { get; }
-
         /// <summary>
-        /// Create a new instance of loot.
+        /// The Loot class should implement IComparable<Loot>
+        /// and override GetHashCode() and Equals()
         /// </summary>
-        /// <param name="kind">Type of loot.</param>
-        /// <param name="description">Loot description.</param>
-        /// <param name="value">Loot value.</param>
-        public Loot(LootType kind, string description, float value)
+        public class Loot : IComparable<Loot>
         {
-            Kind = kind;
-            Description = description;
-            Value = value;
-        }
+            /// <summary>Type of loot.</summary>
+            public LootType Kind { get; }
 
-        /// <summary>
-        /// Return a nicely formatted string representing the loot instance.
-        /// </summary>
-        /// <returns>
-        /// A nicely formatted string representing the loot instance.
-        /// </returns>
-        public override string ToString() =>
-            $"[{Kind,15}]\t{Value:f2}\t{Description}";
+            /// <summary>Loot description.</summary>
+            public string Description { get; }
 
-        /// <summary>
-        /// Compare all the loot and organize them accordingly.
-        /// </summary>
-        /// <param name="other"></param>
-        /// <returns></returns>
-        public int CompareTo(Loot other)
-        {
-            if (other == null) return 1;
-            int typeComparison = string.Compare(Kind.ToString(), 
-            other.Kind.ToString(), StringComparison.Ordinal);
+            /// <summary>Loot value.</summary>
+            public float Value { get; }
 
-            //Compares the type of loot
-            if (typeComparison != 0)
-                return typeComparison;
+            /// <summary>
+            /// Create a new instance of loot.
+            /// </summary>
+            /// <param name="kind">Type of loot.</param>
+            /// <param name="description">Loot description.</param>
+            /// <param name="value">Loot value.</param>
+            public Loot(LootType kind, string description, float value)
+            {
+                Kind = kind;
+                Description = description;
+                Value = value;
+            }
 
-            int valueComparison = Value.CompareTo(other.Value);
-            //Depending on the value comparison:
-            if (valueComparison != 0)
-                //Sort between values.
-                return -valueComparison; 
+            /// <summary>
+            /// Return a nicely formatted string representing the loot instance.
+            /// </summary>
+            /// <returns>
+            /// A nicely formatted string representing the loot instance.
+            /// </returns>
+            public override string ToString() =>
+                $"[{Kind,15}]\t{Value:f2}\t{Description}";
 
-            //If they are the same then sort them alphabetically.
-            return string.Compare(Description, other.Description, 
-            StringComparison.Ordinal);
-        }
+            /// <summary>
+            /// Compare all the loot and organize them accordingly.
+            /// </summary>
+            /// <param name="other"></param>
+            /// <returns></returns>
+            public int CompareTo(Loot other)
+            {
+                if (other == null) return 1;
+                int typeComparison = string.Compare(Kind.ToString(), 
+                other.Kind.ToString(), StringComparison.Ordinal);
 
-        /// <summary>
-        /// Equals override to return overwritten type
-        /// </summary>
-        /// <param name="obj"></param>
-        /// <returns></returns>
-        public override bool Equals(object obj)
-        {
-            //If there is no object then it will get a type.
-            if (obj == null || GetType() != obj.GetType())
-                return false;
+                //Compares the type of loot
+                if (typeComparison != 0)
+                    return typeComparison;
 
-            Loot other = (Loot)obj;
-            return Kind == other.Kind && Value == other.Value 
-            && Description == other.Description;
-        }
-         /// <summary>
-        /// Overrides HashCode and add new value
-        /// </summary>
-        /// <returns></returns>
-        public override int GetHashCode()
-        {
-            //Int Value
-            int hash = 20;
-            //Override value + get kind
-            hash = hash * 35 + Kind.GetHashCode();
-            //Override value + get value
-            hash = hash * 35 + Value.GetHashCode();
-            hash = hash * 35 + (Description != null ? Description.GetHashCode()
-             : 0);
-            return hash; 
+                int valueComparison = Value.CompareTo(other.Value);
+                //Depending on the value comparison:
+                if (valueComparison != 0)
+                    //Sort between values.
+                    return -valueComparison; 
+
+                //If they are the same then sort them alphabetically.
+                return string.Compare(Description, other.Description, 
+                StringComparison.Ordinal);
+            }
+
+            /// <summary>
+            /// Equals override to return overwritten type
+            /// </summary>
+            /// <param name="obj"></param>
+            /// <returns></returns>
+            public override bool Equals(object obj)
+            {
+                //If there is no object then it will get a type.
+                if (obj == null || GetType() != obj.GetType())
+                    return false;
+
+                Loot other = (Loot)obj;
+                return Kind == other.Kind && Value == other.Value 
+                && Description == other.Description;
+            }
+            /// <summary>
+            /// Overrides HashCode and add new value
+            /// </summary>
+            /// <returns></returns>
+            public override int GetHashCode()
+            {
+                //Int Value
+                int hash = 20;
+                //Override value + get kind
+                hash = hash * 35 + Kind.GetHashCode();
+                //Override value + get value
+                hash = hash * 35 + Value.GetHashCode();
+                hash = hash * 35 + (Description != null ? Description.GetHashCode()
+                : 0);
+                return hash; 
+            }
         }
     }
-}
